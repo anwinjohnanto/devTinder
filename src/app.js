7234f9d33fc16d776e68d23264c4fa5d6a16c275
anwinwindfall <anwina@windfallproductions.org>
2024-11-23 15:41:54 +0530

    experimenting next

--- src/app.js
+++ src/app.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const app = express();;
 
-app.use("/hello", (req, res)=>{
-    res.send("Hello World!")
-})
+
+app.use('/users', (req, res, next)=>{
+    console.log("Response 1");
+    // res.send("Response 1");
+    next();
+},
+[(req,res)=>{
+    console.log("Response 2");
+    res.send("Response 2");
+},
+(req,res)=>{
+    console.log("Response 2");
+    res.send("Response 2");
+}]
+)
+
 
 app.listen(7777,()=>{
     console.log("server is listening to port 7777");
