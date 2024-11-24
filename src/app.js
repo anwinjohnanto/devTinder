0e7a9be36b36efc9a8492177b50f1d63c02e048f
anwinwindfall <anwina@windfallproductions.org>
2024-11-24 08:01:33 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -3,22 +3,20 @@ const { adminAuth, userAuth } = require('./middlewares/auth');
 const app = express();;
 
 
-app.use('/admin', adminAuth);
+app.use('/users', (req, res, next) => {
+    try{
 
-app.get('/admin/getAllUsers', (req, res)=>{
-    res.send('get all users');
+        throw new Error('Something went wrong');
+    } catch(err){
+        res.status(500).send('Something went wrong try');
+    }
 })
-app.post('/admin/createUser', (req, res)=>{
-    res.send('post all users');
-})
-
-app.get('/user/dashboard', userAuth, (req, res)=>{
-    res.send('user dashboard');
-})
-app.get('/user/login', (req, res)=>{
-    res.send('user dashboard');
-})
-
+app.use('/', (err, req, res, next) => {
+    if (err) {
+        res.status(500).send('Something went wrong');       
+    }
+    
+});
 
 app.listen(7777,()=>{
     console.log("server is listening to port 7777");
