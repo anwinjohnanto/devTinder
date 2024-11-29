cbc450251aaf7c4e8e0f3430dc12142ed15e985d
anwinwindfall <anwina@windfallproductions.org>
2024-11-29 20:18:44 +0530

    find users from DB

--- src/app.js
+++ src/app.js
@@ -1,23 +1,50 @@
 const express = require('express');
 const { adminAuth, userAuth } = require('./middlewares/auth');
 const connectDB = require('./config/database');
+const userModel = require('./models/user')
 const app = express();;
 
+app.use(express.json());
 
-app.use('/users', (req, res, next) => {
+app.post('/signup', async (req, res)=>{
+    const userObj = new userModel(req.body);
+
     try{
-
-        throw new Error('Something went wrong');
-    } catch(err){
-        res.status(500).send('Something went wrong try');
+        await userObj.save();
+        res.status(201).send('User created successfully');
+    }catch(err){
+        res.status(500).send("Something went wrong");
     }
 })
-app.use('/', (err, req, res, next) => {
-    if (err) {
-        res.status(500).send('Something went wrong');       
-    }
-});
 
+app.get('/user', async (req, res)=>{
+    try{
+        const user = await userModel.findOne({email: req.body.email});        
+        if (!user) {
+            res.status(404).send("User not found");
+        }
+        else{
+            res.status(200).send(user);
+        }
+    }catch(err){
+        res.status(500).send("Something went wrong");
+    }
+    
+})
+
+app.get('/feed', async (req, res)=>{
+    try{
+        const feed = await userModel.find({}).exec();
+        if (feed.length == 0) {
+            res.status(404).send("users not found")
+        }
+        else{
+            res.send(feed)
+        }
+    } catch(e){
+        res.status(500).send("Something went wrong")
+    }
+})
 
 connectDB().then(()=>{
     console.log("Connection to DB established");
